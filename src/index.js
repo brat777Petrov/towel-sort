3260752ff14dce00d664594e8efe6a2ef57ce106
Sasha Petrov <brat77@tut.by>
2021-03-03 21:08:25 +0300

    init: add dicision task

--- src/index.js
+++ src/index.js
@@ -2,5 +2,21 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  if (!matrix ) return [];
+  
+var arr = [];
+var arrL = [];
+  for (var i = 0; i < matrix.length; i++ ) {
+    arr = matrix[i];
+      if ( i%2 ) arr.reverse();
+    for (var j = 0; j < arr.length; j++ ) {
+      arrL.push(arr[j]);
+    };
+    
+    
+  };
+  console.log(arrL);  
+    console.log(typeof(arrL));
+  
+return arrL;
 }
